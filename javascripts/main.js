77dc60ad9a1597bbff037e73c7f45d553d4b2d1c
Oliver Ponder <oliver.ponder@gmail.com>
2015-02-01 23:33:31 +0100

    Initial flattened commit

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+ // some code..
+} else {
+$('#projects-terminal').sticky({
+  context: '#projects',
+  offset:  10
+});		
+}
+
